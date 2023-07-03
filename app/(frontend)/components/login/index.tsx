d33fe18d52eaf55a74720fce2edbe3c7c08df53f
Onur <runotr13@gmail.com>
2023-07-03 10:55:31 +0300

    add

--- app/(frontend)/components/login/index.tsx
+++ app/(frontend)/components/login/index.tsx
@@ -3,17 +3,16 @@ import React, { useEffect } from 'react'
 import { store } from '../../redux/store/store'
 import { darkMode } from '../../redux/actions/darkmode/mode';
 import { login } from '../../redux/actions/auth/authReducer';
-import { signIn, signOut, useSession } from "next-auth/react"
 export default function Login() {
   const { mode } = store.getState().mode;
   const [count, setCount] = React.useState(0);
   const [newMode, setNewMode] = React.useState(mode);
-  const { data: session, status } = useSession()
-  const loading = status === "loading"
+
+
   const changeMode = () => {
     setNewMode(!newMode)
   }
-  console.log("session",session);
+
   useEffect(() => {
     store.dispatch(darkMode(newMode));
   }, [newMode])
@@ -33,18 +32,7 @@ export default function Login() {
     <div>Login Components
       count : {count}
       <div>
-        {!session && (
-          <>
-            Not signed in <br />
-            <button onClick={() => signIn()} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Sign in</button>
-          </>
-        )}
-        {session && (
-          <>
-            {/* Signed in as {session.user.email} <br /> */}
-            <button onClick={() => signOut()} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Sign out</button>
-          </>
-        )}
+       
       </div>
       <div>
         <button onClick={() => changeMode()} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Change Mode</button>
