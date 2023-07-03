7edeb822aae362e4cd2baceb5abcfaeaeffa92e8
Onur <runotr13@gmail.com>
2023-07-03 10:54:55 +0300

    create login

--- app/(frontend)/components/login/index.tsx
+++ app/(frontend)/components/login/index.tsx
@@ -3,34 +3,29 @@ import React, { useEffect } from 'react'
 import { store } from '../../redux/store/store'
 import { darkMode } from '../../redux/actions/darkmode/mode';
 import { login } from '../../redux/actions/auth/authReducer';
+import { signIn, signOut, useSession } from "next-auth/react"
 export default function Login() {
   const { mode } = store.getState().mode;
   const [count, setCount] = React.useState(0);
   const [newMode, setNewMode] = React.useState(mode);
-  console.log('LOGIN PAGE use client')
-  const increase = () => {
-    setCount(count + 1)
-    console.log('count', count)
-  }
-  const decrease = () => {
-    setCount(count - 1)
-    console.log('count', count)
-  }
+  const { data: session, status } = useSession()
+  const loading = status === "loading"
   const changeMode = () => {
     setNewMode(!newMode)
   }
+  console.log("session",session);
   useEffect(() => {
     store.dispatch(darkMode(newMode));
   }, [newMode])
   const getLogin = () => {
     const obj = {
-      name : 'test',
-      surname : 'test'
+      name: 'test',
+      surname: 'test'
     }
     store.dispatch(login(obj)).then((res) => {
-      console.log('res',res)
+      console.log('res', res)
     }).catch((err) => {
-      console.log('err',err)
+      console.log('err', err)
     })
   }
   getLogin()
@@ -38,8 +33,18 @@ export default function Login() {
     <div>Login Components
       count : {count}
       <div>
-        <button onClick={() => increase()} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Arttir</button>
-        <button onClick={() => decrease()} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Azalt</button>
+        {!session && (
+          <>
+            Not signed in <br />
+            <button onClick={() => signIn()} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Sign in</button>
+          </>
+        )}
+        {session && (
+          <>
+            {/* Signed in as {session.user.email} <br /> */}
+            <button onClick={() => signOut()} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Sign out</button>
+          </>
+        )}
       </div>
       <div>
         <button onClick={() => changeMode()} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Change Mode</button>
